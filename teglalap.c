4c4a90f045bf3903186e3b34eba9ff85059ef545
ardvin <49413892+Ardvinx@users.noreply.github.com>
2022-02-13 20:56:49 +0100

    Add files via upload

---
+++ teglalap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    system("clear");
+
+    int SideA = 0;
+    int sideB = 0;
+
+    printf("Ez a program egy téglalap kerületét és területét számolja ki!\n");
+    printf("Adja meg a téglalap A oldalának hosszát (cm): ");
+    scanf("%d",&SideA);
+
+    printf("Adja meg a téglalap B oldalának hosszát (cm): ");
+    scanf("%d",&sideB);
+
+    printf("\nA téglalap kerülete: %dcm, a területe: %dcm2\n",(SideA+sideB)*2,SideA*sideB);
+
+    return 0;
+}
